bd7d81bc48562893d8836ca0dd9be56704041c72
AkhtarAbbas <akhtarabbas9912@gmail.com>
2021-05-11 23:15:00 +0500

    form-wizard and model changes

--- docs/.vuepress/components/DemoTabs.vue
+++ docs/.vuepress/components/DemoTabs.vue
@@ -2,13 +2,13 @@
   <div>
     <cap-tabs>
       <cap-tab-pane name="Tab one" :selected="true">
-        <span>this is tab one.</span>
+        <span>this is tab one</span>
       </cap-tab-pane>
       <cap-tab-pane name="Tab two" :selected="false">
-        <span>this is tab two.</span>
+        <span>this is tab two</span>
       </cap-tab-pane>
       <cap-tab-pane name="Tab three" :selected="false">
-        <span>this is tab three.</span>
+        <span>this is tab three</span>
       </cap-tab-pane>
     </cap-tabs>
   </div>
