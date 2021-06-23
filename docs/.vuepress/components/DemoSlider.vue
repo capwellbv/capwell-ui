a89e4d1aec44b39cee2f2eb70889b71fe6dc9057
AkhtarAbbas <akhtarabbas9912@gmail.com>
2021-06-23 13:05:37 +0500

    slider styles updated

--- docs/.vuepress/components/DemoSlider.vue
+++ docs/.vuepress/components/DemoSlider.vue
@@ -10,20 +10,20 @@ export default {
   data() {
     return {
       items: [
-        {slug: "#", title: "ROTTERDAM", description: "description....." },
+        {slug: "#", title: "ROTTERDAM", description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus quaerat perferendis itaque aliquid, repellendus quas dignissimos dolorem tenetur, atque debitis, quis doloremque quisquam repellat expedita fugit facere. Culpa, deleniti perspiciatis!" },
         {
                     slug: "#",
           title: "HILVERSUM",
           description: "Lorem ipsum dolor ",
         },
-        {slug: "#", title: "DUIVEN", description: "description....." },
-        { slug: "#", title: "ROTTERDAM", description: "description....." },
+        {slug: "#", title: "DUIVEN", description: "repellendus quas dignissimos dolorem tenetur, atque debitis, quis doloremque quisquam repellat expedita fugit facere. Culpa, deleniti perspiciatis!" },
+        { slug: "#", title: "ROTTERDAM", description: "quis doloremque quisquam repellat expedita fugit facere. Culpa, deleniti perspiciatis!" },
         {
                     slug: "#",
           title: "DUIVeN",
           description: "Lorem ipsum dolor ",
         },
-        {slug: "#", title: "HILVERSUM", description: "description....." },
+        {slug: "#", title: "HILVERSUM", description: "quis doloremque quisquam repellat expedita fugit facere.!" },
       ],
     };
   },
